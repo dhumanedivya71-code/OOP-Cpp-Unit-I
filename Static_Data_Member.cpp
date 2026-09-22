33214bf5005a4ff4079e7fb663593d6ad9f7114a
Divya Dhumane <dhumanedivya71@gmail.com>
2026-09-21 23:13:56 -0700

    Implement static data member in Student class

---
+++ Static_Data_Member.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Student{
+    public:
+    static int count;
+    Student(){
+        count++;
+    }
+};
+int Student::count=0;
+int main()
+{
+    Student s1, s2, s3;
+    cout<<Student::count;
+    return 0;
+}
